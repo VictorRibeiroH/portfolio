0841371793ed7caa767272df0d5c529618dcc026
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-09-26 21:02:24 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Victor Ribeiro Portfolio</title>
+      </Head>
+
+      <h1>meu Portfolio</h1>
+    </div>
+  );
+};
+
+export default Home;
